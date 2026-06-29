d4aa4fec45f7fe04fffa550a46b5775932de2821
Hugo Cruz <hugocruzlfc@gmail.com>
2026-06-29 18:16:38 +0200

    feat: add project scaffolding

---
+++ src/user/user.controller.ts
@@ -0,0 +1,57 @@
+import { Body, Controller, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+// @Get('all')    // GET /user/all
+// @Get(':id')    // GET /user/:id. ------dynamic segment
+// @Post()        // POST /user
+// @Put(':id')    // PUT /user/:id
+// @Delete(':id')    // Delete /user/:id
+
+// rutas staticas primero q las dinamicas
+
+@Controller('user')
+export class UserController {
+  // @Query:
+  // localhost:3000/user?name=Hugo
+  @Get()
+  getUsers(@Query('name') name: string) {
+    const users = [
+      { id: 1, name: 'John Doe' },
+      { id: 2, name: 'Hugo' },
+    ];
+
+    if (name) {
+      return users.filter((user) =>
+        user.name.toLowerCase().includes(name.toLocaleLowerCase()),
+      );
+    }
+
+    return users;
+  }
+
+  // @Param:
+  // localhost:3000/user/123
+
+  @Get(':id')
+  getUserById(@Param('id') id: string) {
+    return { id, name: 'John Doe' };
+  }
+
+  // @Body:
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return { data: createUserDto, message: 'User created successfully' };
+  }
+
+  @Put(':id')
+  updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return {
+      data: {
+        id,
+        ...updateUserDto,
+      },
+      message: 'User updated successfully',
+    };
+  }
+}
